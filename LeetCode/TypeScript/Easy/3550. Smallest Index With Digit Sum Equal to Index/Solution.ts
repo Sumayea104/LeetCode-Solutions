db4efc9d85c46db032b6f88b3260709e90c9edf7
Sumayea Rahman <shorbonam104@gmail.com>
2026-09-24 08:16:37 +0600

    Add Easy TypeScript Solution for 3550. Smallest Index With Digit Sum Equal to Index from LeetCode

---
+++ LeetCode/TypeScript/Easy/3550. Smallest Index With Digit Sum Equal to Index/Solution.ts
@@ -0,0 +1,17 @@
+function smallestIndex(nums: number[]): number {
+    for (let i = 0; i < nums.length; i++) {
+        let sum = 0;
+        let num = nums[i];
+        
+        while (num > 0) {
+            sum += num % 10;
+            num = Math.floor(num / 10);
+        }
+        
+        if (sum === i) {
+            return i;
+        }
+    }
+    
+    return -1;
+}
